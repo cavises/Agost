229bd4b8bf12b616c914ca0dd32c517410a01d47
cavises <138293452+cavises@users.noreply.github.com>
2023-08-22 22:11:10 +0200

    Update C02+.c

--- C02+.c
+++ C02+.c
@@ -23,5 +23,21 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	return (end_src);
 }
 
+int				main(void)
+{
+	char	*string1;
+	char	string2[20];
+	int   length_src;
+
+	string1 = "Hello 42";
+	printf("base   : %s\n", string1);
+	//length_src = strlcpy(string2, string1, 4);
+	//printf("cpy c  : %s\n", string2);
+	//printf("cpy c (nb) : %d\n", length_src);
+	length_src = ft_strlcpy(string2, string1, 4);
+	printf("cpy ft : %s\n", string2);
+	printf("cpy ft (nb) : %d\n", length_src);
+}
+
 ex11
 
