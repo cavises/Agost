1c7c99169f7cb4489e6a12a12fbfe1ef63b5782f
cavises <138293452+cavises@users.noreply.github.com>
2023-08-22 10:14:58 +0200

    Update C03+.c

--- C03+.c
+++ C03+.c
@@ -27,3 +27,31 @@ int	main(void)
 
 ex01
 
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	while (*s1 != '\0' && (*s1 == *s2) && n > 0)
+	{
+		s1++;
+		s2++;
+		n--;
+	}
+	return (*(unsigned char*)s1 - *(unsigned char*)s2);
+}
+
+int	main(void)
+{
+	char *str1;
+	char *str2;
+
+	str1 = "Helloo";
+	str2 = "Heloo";
+	printf("c  : %d\n", strncmp(str1, str2, 5));
+	printf("ft : %d\n", ft_strncmp(str1, str2, 5));
+}
+
+
+ex02
+
