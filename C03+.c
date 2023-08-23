65d3c9825747959130d6cda694aeb0f09f524203
cavises <138293452+cavises@users.noreply.github.com>
2023-08-23 10:38:58 +0200

    Update C03+.c

--- C03+.c
+++ C03+.c
@@ -146,3 +146,63 @@ int		main(void)
 
 ex04
 
+#include <stdio.h>
+#include <string.h>
+
+int	ft_complete_search(int i_str, int i_find, char *str, char *to_find)
+{
+	int	i_next;
+
+	i_next = 0;
+	while (str[i_str + i_next] == to_find[i_next])
+	{
+		if (i_next == (i_find - 1))
+			return (1);
+		else if (i_next < i_find)
+			i_next++;
+	}
+	return (0);
+}
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int			i_str;
+	int			i_find;
+	char		*p;
+
+	i_str = 0;
+	i_find = 0;
+	while (to_find[i_find])
+		i_find++;
+	if (i_find > 0)
+	{
+		while (str[i_str])
+		{
+			if (str[i_str] == to_find[0])
+			{
+				if (ft_complete_search(i_str, i_find, str, to_find))
+					return (p = &str[i_str]);
+			}
+			i_str++;
+		}
+	}
+	else
+		return (p = &str[0]);
+	return (0);
+}
+
+int		main(void)
+{
+	char *src;
+	char *to_find;
+	char *result_c;
+	char *result_ft;
+
+	src = "Foo Fighters";
+	to_find = "Fight";
+	result_c = strstr(src, to_find);
+	result_ft = ft_strstr(src, to_find);
+	printf("%p / %p\n", result_c, result_ft);
+	printf("c  : %s$\n", result_c);
+	printf("ft : %s$\n", result_ft);
+}
