4aed3bf71844080543e4fba9094c1c566a170ca3
cavises <138293452+cavises@users.noreply.github.com>
2023-08-23 09:01:47 +0200

    Update C02+.c

--- C02+.c
+++ C02+.c
@@ -1,6 +1,38 @@
 ex01
 
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
 
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int		main(void)
+{
+	char *src;
+	char dest[20];
+
+	src = "Hello World";
+	printf("base   : %s\n", src);
+	strncpy(dest, src, 8);
+	printf("cpy    : %s\n", dest);
+	ft_strncpy(dest, src, 8);
+	printf("ft_cpy : %s\n", dest);
+}
 
 ex10
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
