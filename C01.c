8208e15b60c4f7ad7307485ac1f533648b2bd080
cavises <138293452+cavises@users.noreply.github.com>
2023-08-24 14:53:50 +0200

    Update C01.c

--- C01.c
+++ C01.c
@@ -9,23 +9,18 @@ void	ft_ft(int *nbr)
 	*nbr = 42;
 }
 
-/*
 #include <stdio.h>
 
-void	ft_ft(int *nbr)
+int	main(void)
 {
-	*nbr = 42;
-}
+	int	a;
 
-int		main(void)
-{
-	int a;
-
-	a = 1;
+	a = 333;
 	printf("before: %d\n", a);
 	ft_ft(&a);
 	printf("after : %d\n", a);
-}*/
+}
+
 
 /* =========================================================================================================
   EXERCICI 01
@@ -36,28 +31,22 @@ void	ft_ultimate_ft(int *********nbr)
 	*********nbr = 42;
 }
 
-/*
 #include <stdio.h>
 
-void	ft_ultimate_ft(int *********nbr)
+int	main(void)
 {
-	*********nbr = 42;
-}
+	int	a;
+	int	*ptr1;
+	int	**ptr2;
+	int	***ptr3;
+	int	****ptr4;
+	int	*****ptr5;
+	int	******ptr6;
+	int	*******ptr7;
+	int	********ptr8;
+	int	*********ptr9;
 
-int		main(void)
-{
-	int a;
-	int *ptr1;
-	int **ptr2;
-	int ***ptr3;
-	int ****ptr4;
-	int *****ptr5;
-	int ******ptr6;
-	int *******ptr7;
-	int ********ptr8;
-	int *********ptr9;
-
-	a = 1;
+	a = 999;
 	ptr1 = &a;
 	ptr2 = &ptr1;
 	ptr3 = &ptr2;
@@ -68,9 +57,22 @@ int		main(void)
 	ptr8 = &ptr7;
 	ptr9 = &ptr8;
 	printf("before: %d\n", a);
+	printf("before a address: %p\n", &a);
+	printf("before ptr1 aim to address: %p\n", ptr1);
+	printf("before ptr1 own address: %p\n", &ptr1);
+	printf("before ptr2 aim to address: %p\n", ptr2);
+	printf("before ptr3 aim to address: %p\n", ptr3);
+	printf("before ptr9 aim to address: %p\n", ptr9);
 	ft_ultimate_ft(ptr9);
 	printf("after : %d\n", a);
-}*/
+	printf("after a adress: %p\n", &a);
+	printf("after ptr1 aim to address: %p\n", ptr1);
+	printf("after ptr1 own address: %p\n", &ptr1);
+	printf("after ptr2 aim to address: %p\n", ptr2);
+	printf("after ptr3 aim to address: %p\n", ptr3);
+	printf("after ptr9 aim to address: %p\n", ptr9);
+}
+
 
 /* =========================================================================================================
   EXERCICI 02
@@ -84,17 +86,7 @@ void	ft_swap(int *a, int *b)
 	*b = c;
 }
 
-/*
 #include <stdio.h>
-
-void	ft_swap(int *a, int *b)
-{
-	int c;
-
-	c = *a;
-	*a = *b;
-	*b = c;
-}
 
 int		main(void)
 {
@@ -106,7 +98,8 @@ int		main(void)
 	printf("before: %d %d\n", a, b);
 	ft_swap(&a, &b);
 	printf("after: %d %d\n", a, b);
-}*/
+}
+
 
 
 /* =========================================================================================================
@@ -118,14 +111,7 @@ void	ft_div_mod(int a, int b, int *div, int *mod)
 	*mod = a % b;
 }
 
-/*
 #include <stdio.h>
-
-void	ft_div_mod(int a, int b, int *div, int *mod)
-{
-	*div = a / b;
-	*mod = a % b;
-}
 
 int		main(void)
 {
@@ -137,8 +123,9 @@ int		main(void)
 	a = 5;
 	b = 2;
 	ft_div_mod(a, b, &div, &mod);
-	printf("%d / %d = %d, left %d\n", a, b, div, mod);
-}*/
+	printf("%d / %d = %d, remainder %d\n", a, b, div, mod);
+}
+
 
 
 /* =========================================================================================================
@@ -156,19 +143,7 @@ void	ft_ultimate_div_mod(int *a, int *b)
 	*b = mod;
 }
 
-/*
 #include <stdio.h>
-
-void	ft_ultimate_div_mod(int *a, int *b)
-{
-	int div;
-	int mod;
-
-	div = *a / *b;
-	mod = *a % *b;
-	*a = div;
-	*b = mod;
-}
 
 int		main(void)
 {
@@ -178,8 +153,8 @@ int		main(void)
 	a = 5;
 	b = 2;
 	ft_ultimate_div_mod(&a, &b);
-	printf("div %d, left %d", a, b);
-}*/
+	printf("div %d, remainder %d", a, b);
+}
 
 
 /* =========================================================================================================
@@ -210,56 +185,17 @@ void	ft_putstr(char *str)
 	}
 }
 
-/*
-#include <unistd.h>
-#include <stdbool.h>
 #include <stdio.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	char next_char;
-
-	while (true)
-	{
-		next_char = *str;
-		if (next_char == '\0')
-		{
-			break ;
-		}
-		ft_putchar(next_char);
-		str++;
-	}
-}
 
 int		main(void)
 {
-	char string[15];
+	char stri[] = "Hello World again 42";
 	char *first_pointer;
-
-	string[0] = 'H';
-	string[1] = 'e';
-	string[2] = 'l';
-	string[3] = 'l';
-	string[4] = 'o';
-	string[5] = ' ';
-	string[6] = 'W';
-	string[7] = 'o';
-	string[8] = 'r';
-	string[9] = 'l';
-	string[10] = 'd';
-	string[11] = ' ';
-	string[12] = '4';
-	string[13] = '2';
-	string[14] = '\0';
-	first_pointer = &string[0];
-	printf("%s\n", string);
+	
+	first_pointer = &stri[0];
 	ft_putstr(first_pointer);
-}*/
+	printf("\nEntrada : %s\n", stri);
+}
 
 
 /* =========================================================================================================
@@ -278,46 +214,20 @@ int		ft_strlen(char *str)
 	return (length);
 }
 
-/*
 #include <stdio.h>
 
-int		ft_strlen(char *str)
-{
-	int length;
-
-	length = 0;
-	while (*(str++) != '\0')
-	{
-		length++;
-	}
-	return (length);
-}
 
 int		main(void)
 {
-	char	string[15];
+	char	string[] = "Hello World 42 again and again";
 	char	*first_pointer;
 	int		length;
 
-	string[0] = 'H';
-	string[1] = 'e';
-	string[2] = 'l';
-	string[3] = 'l';
-	string[4] = 'o';
-	string[5] = ' ';
-	string[6] = 'W';
-	string[7] = 'o';
-	string[8] = 'r';
-	string[9] = 'l';
-	string[10] = 'd';
-	string[11] = ' ';
-	string[12] = '4';
-	string[13] = '2';
-	string[14] = '\0';
 	first_pointer = &string[0];
 	length = ft_strlen(first_pointer);
 	printf("length %d\n", length);
-}*/
+}
+
 
 
 /* =========================================================================================================
@@ -339,25 +249,9 @@ void	ft_rev_int_tab(int *tab, int size)
 	}
 }
 
-/*
 #include <stdio.h>
 
-void	ft_rev_int_tab(int *tab, int size)
-{
-	int index;
-	int tmp;
-
-	index = 0;
-	while (index < size / 2)
-	{
-		tmp = tab[index];
-		tab[index] = tab[size - 1 - index];
-		tab[size - 1 - index] = tmp;
-		index++;
-	}
-}
-
-void	debug_dump_array(int numbers[], int size)
+void	print_array(int numbers[], int size)
 {
 	int index;
 
@@ -377,23 +271,26 @@ void	debug_dump_array(int numbers[], int size)
 
 int		main(void)
 {
-	int numbers[9];
+	int numbers[10];
 	int *first_pointer;
-	int index;
+	int val;
+	int i;
 
-	index = 0;
-	while (index < 9)
+	val = 0;
+	i = 0;
+	while (i <= 9)
 	{
-		numbers[index] = index + 1;
-		index++;
+		numbers[i] = val;
+		i++;
+		val++;
 	}
 	first_pointer = &numbers[0];
 	printf("before: ");
-	debug_dump_array(numbers, 9);
-	ft_rev_int_tab(first_pointer, 9);
+	print_array(numbers, 10);
+	ft_rev_int_tab(first_pointer, 10);
 	printf("\nafter : ");
-	debug_dump_array(numbers, 9);
-}*/
+	print_array(numbers, 10);
+}
 
 
 /* =========================================================================================================
@@ -439,48 +336,9 @@ void	ft_sort_int_tab(int *tab, int size)
 	}
 }
 
-/*
-#include <stdbool.h>
 #include <stdio.h>
 
-void	ft_swap(int *a, int *b)
-{
-	int c;
-
-	c = *a;
-	*a = *b;
-	*b = c;
-}
-
-void	ft_sort_int_tab(int *tab, int size)
-{
-	int		read_index;
-	int		index;
-	bool	swapped;
-
-	read_index = 0;
-	while (true)
-	{
-		swapped = false;
-		index = 0;
-		while (index < size)
-		{
-			if (tab[index] > tab[index + 1])
-			{
-				ft_swap(&tab[index], &tab[index + 1]);
-				swapped = true;
-			}
-			index++;
-		}
-		if (!swapped)
-		{
-			break ;
-		}
-		read_index++;
-	}
-}
-
-void	debug_dump_array(int numbers[], int size)
+void	print_array(int numbers[], int size)
 {
 	int index;
 
@@ -500,20 +358,14 @@ void	debug_dump_array(int numbers[], int size)
 
 int		main(void)
 {
-	int numbers[9];
+	int numbers[9] = {-2, 0, 4, 12, 14, 8, 10, 2, 6};
 	int *first_pointer;
-	int index;
-
-	index = 0;
-	while (index < 9)
-	{
-		numbers[index] = 9 - (index + 1);
-		index++;
-	}
+	
 	first_pointer = &numbers[0];
 	printf("before: ");
-	debug_dump_array(numbers, 9);
+	print_array(numbers, 9);
 	ft_sort_int_tab(first_pointer, 9);
 	printf("\nafter : ");
-	debug_dump_array(numbers, 9);
-}*/
+	print_array(numbers, 9);
+}
+
