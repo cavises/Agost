455d46f34b85666e755f83f8088dc2927fb02a5f
cavises <138293452+cavises@users.noreply.github.com>
2023-08-24 09:31:58 +0200

    Create C04+.c

---
+++ C04+.c
@@ -0,0 +1,26 @@
+ex00
+
+
+ex01
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int		main(void)
+{
+	char *str;
+
+	str = "Hello 424242";
+	ft_putstr(str);
+}
