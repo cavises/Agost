c0290e5f9d27ee3c555e572e2dda2f7e5d31c9a8
cavises <138293452+cavises@users.noreply.github.com>
2023-08-25 12:06:44 +0200

    Update C06.c

--- C06.c
+++ C06.c
@@ -39,6 +39,20 @@ int	main(int size, char **arg)
 	return (0);
 }
 
+++++++++++++++++++++
+int		main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (++i < argc)
+	{
+		write(1, argv[i]++, 1);
+		write(1, "\n", 1);
+	}
+}
+
+
 
 
 ex02
@@ -62,6 +76,19 @@ int	main(int size, char **args)
 			write(1, "\n", 1);
 			o--;
 		}
+	}
+}
+
+++++++++++++++++++++++
+int		main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (++i < argc)
+	{
+		write(1, argv[argc - i]++, 1);
+		write(1, "\n", 1);
 	}
 }
 
@@ -120,4 +147,71 @@ int	main(int size, char **args)
 		o++;
 	}
 	ft_putchar(args, size, 1);
+}
+
+++++++++++++++++++++++
+#include <stdbool.h>
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char *c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char*)s1 - *(unsigned char*)s2);
+}
+
+void	ft_str_sort(char **arr, int size, int offset)
+{
+	int		index;
+	bool	swapped;
+
+	index = offset;
+	while (true)
+	{
+		index = offset;
+		swapped = false;
+		while (index < size)
+		{
+			if (ft_strcmp(arr[index], arr[index + 1]) > 0)
+			{
+				ft_swap(&arr[index], &arr[index + 1]);
+				swapped = true;
+			}
+			index++;
+		}
+		if (!swapped)
+			break ;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int	index;
+
+	if (argc > 2)
+		ft_str_sort(argv, argc - 1, 1);
+	index = 0;
+	while (++index < argc)
+	{
+		ft_putstr(argv[index]);
+		ft_putstr("\n");
+	}
 }
