d2933c2e1790f03edbf4ffa0aa51aedf1ed811b8
cavises <138293452+cavises@users.noreply.github.com>
2023-08-25 07:53:14 +0200

    Update C04+.c

--- C04+.c
+++ C04+.c
@@ -93,3 +93,79 @@ int	main()
 
 ex03
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_string(char c, char *str)
+{
+	while (*str)
+	{
+		if (*str == c)
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+int	ft_isspace(char c)
+{
+	if (ft_string(c, "\t\n\v\f\r ") == 1)
+	  return (1);
+  else
+    return (0);
+}
+
+int	ft_isoperator(char c)
+{
+	if (ft_string(c, "+-") == 1)
+	  return (1);
+  else
+    return (0);
+}
+
+int	ft_isnumber(char c)
+{
+	if (c >= '0' && c <= '9')
+	  return (1);
+  else
+    return (0);
+}
+
+int		ft_atoi(char *str)
+{
+	int	output;
+	int	sign;
+
+	output = 0;
+	sign = 1;
+	while (ft_isspace(*str) == 1)
+		str++;
+	while (ft_isoperator(*str) == 1)
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (ft_isnumber(*str) == 1)
+	{
+		output *= 10;
+		output += *str - 48;
+		str++;
+	}
+	return (output * sign);
+}
+
+int		main(void)
+{
+	char *str;
+	char *str2;
+
+	str = " ---+--+1234ab567";
+	printf("%d\n", ft_atoi(str));
+	str2 = " -1234ab567";
+	printf("%d\n", atoi(str2));
+}
+
+
+ex04
+
