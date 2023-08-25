b15c3927eaf9b46fad494294210192243040076d
cavises <138293452+cavises@users.noreply.github.com>
2023-08-25 10:22:36 +0200

    Update C05+.c

--- C05+.c
+++ C05+.c
@@ -1,1 +1,37 @@
+ex00
+
+#include <stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	factorial;
+	int	i;
+
+	if (nb < 0)
+		return (0);
+	i = 1;
+	factorial = 1;
+	while (i <= nb)
+	{
+		factorial = factorial * i;
+		i++;
+	}
+	return (factorial);
+}
+
+int	main(void)
+{
+	int n;
+
+	n = -2;
+	while (n < 7)
+	{
+		printf("fact(%d) = %d\n", n, ft_iterative_factorial(n));
+		n++;
+	}
+}
+
+
+ex01
+
 
