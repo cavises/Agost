be56e827b6abf5a4f300341afdf89545e6fc83ac
cavises <138293452+cavises@users.noreply.github.com>
2023-08-25 10:31:02 +0200

    Update C05+.c

--- C05+.c
+++ C05+.c
@@ -60,4 +60,34 @@ int	main(void)
 
 ex02
 
+#include <stdio.h>
+
+int	ft_iterative_power(int nb, int power)
+{
+	int output;
+	int i;
+
+  output = 1;
+  i = 0;
+	if (power < 0)
+		return (0);
+	while (i < power)
+	{
+		output *= nb;
+		i++;
+	}
+	return (output);
+}
+
+int	main(void)
+{
+	printf("10^2 = %d\n", ft_iterative_power(10, 2));
+	printf("10^0 = %d\n", ft_iterative_power(10, 0));
+	printf("10^-1 = %d\n", ft_iterative_power(10, -1));
+	printf("10^5 = %d\n", ft_iterative_power(10, 5));
+}
+
+
+ex03
+
 
