7b270bfbf757d3a4aa28bd43175a0d90a5e7cb70
cavises <138293452+cavises@users.noreply.github.com>
2023-08-25 10:43:37 +0200

    Update C05+.c

--- C05+.c
+++ C05+.c
@@ -113,4 +113,30 @@ int	main(void)
 
 ex04
 
+#include <stdio.h>
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index <= 1)
+		return (index);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+
+int	main(void)
+{
+	int	index;
+
+	index = -2;
+	while (index < 16)
+	{
+		printf("fibonacci(%d) = %d\n", index, ft_fibonacci(index));
+		index++;
+	}
+}
+
+
+ex05
+
 
