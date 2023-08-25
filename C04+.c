1731dac0f0c54e64466a81408d982f717f0958f9
cavises <138293452+cavises@users.noreply.github.com>
2023-08-25 08:16:13 +0200

    Update C04+.c

--- C04+.c
+++ C04+.c
@@ -169,3 +169,75 @@ int		main(void)
 
 ex04
 
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_do_recursive_base(int nbr, char *base, int size)
+{
+	unsigned char	a;
+	unsigned int	n;
+
+	if (nbr < 0)
+	{
+		write(1, "-", 1);
+		n = nbr * (-1);
+	}
+	else
+		n = nbr;
+	if (n >= (unsigned int)size)
+		ft_do_recursive_base(n / size, base, size);
+	a = base[n % size];
+	write(1, &a, 1);
+}
+
+int	ft_verify_base(char *base)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	j = 1;
+	while (base[i] != '\0')
+	{
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		while (base[j] != '\0')
+		{
+		  if (base[i] == base[j])
+		    return (0);
+		  j++;
+		}
+		i++;
+	}
+	if (i <= 1)
+		return (0);
+	return (1);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int	s;
+
+	s = 0;
+	if (ft_verify_base(base) == 1)
+	{
+		while (base[s] != '\0')
+			s++;
+		ft_do_recursive_base(nbr, base, s);
+	}
+}
+
+int		main(void)
+{
+	ft_putnbr_base(-123456, "helo");
+	printf("\n");
+	fflush(stdout);
+	ft_putnbr_base(-2147483648, "0123456789");
+	printf("\n");
+	fflush(stdout);
+	ft_putnbr_base(2147483647, "0123456789");
+}
+
+
+ex05
+
