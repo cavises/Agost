138d7bc15cb1cc6fa9a4fd8d464e944bd771b248
cavises <138293452+cavises@users.noreply.github.com>
2023-08-25 10:26:43 +0200

    Update C05+.c

--- C05+.c
+++ C05+.c
@@ -34,4 +34,30 @@ int	main(void)
 
 ex01
 
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+
+int	main(void)
+{
+	int n;
+
+	n = -2;
+	while (n < 7)
+	{
+		printf("fact(%d) = %d\n", n, ft_recursive_factorial(n));
+		n++;
+	}
+}
+
+
+ex02
+
 
