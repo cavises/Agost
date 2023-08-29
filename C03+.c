33dbd54977d53472c230bf97d137b43f5ebba7ea
cavises <138293452+cavises@users.noreply.github.com>
2023-08-29 05:24:02 +0200

    Update C03+.c

--- C03+.c
+++ C03+.c
@@ -41,6 +41,21 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	return (*(unsigned char*)s1 - *(unsigned char*)s2);
 }
 
+//versio2
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
 int	main(void)
 {
 	char *str1;
